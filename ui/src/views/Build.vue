82681d88aaac00d60f1035153235f8bf527995c5
yinfxs <yinfxs@gmail.com>
2018-09-20 23:58:56 +0800

    update

--- ui/src/views/Build.vue
+++ ui/src/views/Build.vue
@@ -3,93 +3,122 @@
     <van-nav-bar
       :title="this.repo || 'Build List'"
       left-arrow
+      fixed
       @click-left='handleGoback'
-      @click-right='fetchBuilds'
     >
-      <van-icon name="home" slot="right" />
     </van-nav-bar>
-    <van-list>
-      <van-card
-        v-for="build in builds" :key="build.id"
-        :thumb="build.author_avatar"
-        currency=""
-      >
-        <div slot="title" class="van-card__title" style="display: flex;align-items: center;">
-          <span style="margin-right: 8px; display: flex; align-items: center;">
-            <van-icon name="passed" v-if="build.status === 'success'" style="color: #4dc89a" />
-            <van-icon name="close" v-else-if="build.status === 'failure'" style="color: #fc4758" />
-            <van-icon name="logistics" v-else-if="build.status === 'running'" style="color: #38f" />
-            <van-icon name="clock" v-else style="color: #fdb835" />
-          </span>
-          {{build.author}}: {{build.branch}}
-        </div>
-        <p slot="desc" style="color: #666; font-size: 12px;">
-          {{build.message}}
-        </p>
-        <template slot="footer">
-          <van-button type="primary" size="small" plain @click="handleRetry(build.number)" v-if="build.status !== 'running'" :loading="loadingState(build.number)">重试</van-button>
-          <van-button type="danger" size="small" plain @click="handleStop(build.number)" v-if="build.status === 'running'" :loading="loadingState(build.number)">停止</van-button>
-        </template>
-        <template slot="tags">
-          <van-tag type="primary" mark>{{build.number}}</van-tag>
-          <van-tag mark>{{build.created_at | unixDateTime}}</van-tag>
-        </template>
-      </van-card>
-    </van-list>
+    <van-pull-refresh v-model="isLoading" @refresh="fetchBuilds" style="padding-top: 46px;">
+      <van-list>
+        <van-card
+          v-for="build in builds" :key="build.id"
+          :thumb="build.author_avatar"
+          currency=""
+        >
+          <div slot="title" class="van-card__title" style="display: flex; align-items: center;">
+            <span style="margin-right: 8px; display: flex; align-items: center;">
+              <span style="margin-right: 5px;">{{build.number}}</span>
+              <template>
+                <van-icon name="passed" v-if="build.status === 'success'" style="color: #4dc89a" />
+                <van-icon name="close" v-else-if="build.status === 'failure'" style="color: #fc4758" />
+                <van-icon name="logistics" v-else-if="build.status === 'running'" style="color: #38f" />
+                <van-icon name="clock" v-else style="color: #fdb835" />
+              </template>
+            </span>
+            {{build.author}}:{{build.branch}}
+          </div>
+          <p slot="desc" style="color: #666; font-size: 12px;">
+            {{build.message}}
+          </p>
+          <template slot="footer">
+            <van-button
+              type="primary"
+              size="small"
+              @click="handleRetry(build.number)"
+              v-if="build.status !== 'running'"
+              :loading="build.number === loadingBuildId"
+              plain
+              style="border: 1px solid #4A79DC; color: #4A79DC;"
+            >
+              重试
+            </van-button>
+            <van-button
+              type="danger"
+              size="small"
+              @click="handleStop(build.number)"
+              v-if="build.status === 'running'"
+              :loading="build.number === loadingBuildId"
+              plain
+              style="border: 1px solid #F25E56; color: #F25E56;"
+            >
+              停止
+            </van-button>
+          </template>
+          <template slot="tags">
+            <span style="color: #bdbdbd; font-size: 12px; margin-right: 2px;">{{build.created_at | unixDateTime}}</span>
+            <van-tag plain type="primary">
+              {{calcTime(build.started_at, build.finished_at)}}
+            </van-tag>
+          </template>
+        </van-card>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import moment from 'moment'
 import _ from 'lodash'
-import { NavBar, Toast, List, Panel, Card, Icon, Button, Tag } from 'vant'
+import { NavBar, List, Panel, Card, Icon, Button, Tag, PullRefresh } from 'vant'
 import { getReposBuilds, postReposBuilds, deleteReposBuilds } from '@/api/build'
-Vue.use(NavBar).use(List).use(Panel).use(Card).use(Icon).use(Button).use(Tag)
+Vue.use(NavBar).use(List).use(Panel).use(Card).use(Icon).use(Button).use(Tag).use(PullRefresh)
 
 export default {
   name: 'home',
   data () {
     return {
-      loading: {},
+      isLoading: false,
+      loadingBuildId: null,
       activeNames: ['1'],
       builds: []
     }
   },
   methods: {
-    loadingState: function (buildId) {
-      const state = !!this.loading[buildId]
-      console.log(state)
-      return state
+    calcTime: function (startedAt, finishedAt) {
+      if (startedAt && finishedAt) {
+        const time = moment.unix(finishedAt).diff(moment.unix(startedAt), 's')
+        return `${time}s`
+      }
+      return '-'
     },
     fetchBuilds: async function () {
-      Toast.loading({
-        mask: true,
-        duration: 0,
-        forbidClick: true
-      })
+      this.isLoading = true
       this.owner = this.$route.query.owner
       this.repo = this.$route.query.repo
       if (!this.owner || !this.repo) {
         return
       }
-      const builds = await getReposBuilds(this.owner, this.repo)
-      this.builds = builds
-      Toast.clear()
+      this.builds = await getReposBuilds(this.owner, this.repo)
+      const $this = this
+      _.debounce(() => {
+        $this.isLoading = false
+        $this.loadingBuildId = null
+      }, 500)()
     },
     handleGoback: function () {
       this.$router.back()
     },
     handleRetry: async function (buildId) {
-      this.loading[buildId] = true
+      this.loadingBuildId = buildId
       await postReposBuilds(this.owner, this.repo, buildId)
       await _.debounce(this.fetchBuilds, 1000)()
-      this.loading[buildId] = false
+      this.loadingBuildId = null
     },
     handleStop: async function (buildId) {
-      this.loading[buildId] = true
+      this.loadingBuildId = buildId
       await deleteReposBuilds(this.owner, this.repo, buildId)
       await _.debounce(this.fetchBuilds, 1000)()
-      this.loading[buildId] = false
+      this.loadingBuildId = null
     }
   },
   created: function () {
@@ -100,3 +129,9 @@ export default {
   }
 }
 </script>
+<style scoped lang="less">
+:global(.van-card) {
+  height: auto;
+  min-height: 100px;
+}
+</style>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -2,45 +2,50 @@
   <div class='home'>
     <van-nav-bar
       title="Repo List"
-      @click-right='fetchRepos'
+      fixed
     >
-      <van-icon name="home" slot="right" />
     </van-nav-bar>
-    <van-collapse v-model='activeNames'>
-      <van-collapse-item v-for="(list, owner) in repos" :title='owner' :name='owner' :key='owner'>
-        <router-link v-for="repo in list" :to="{ name: 'build', query: { owner, repo: repo.name } }" :key='repo.id'>
-          <van-cell :title="repo.name" is-link/>
-        </router-link>
-      </van-collapse-item>
-    </van-collapse>
+    <van-pull-refresh v-model="isLoading" @refresh="fetchRepos" style="padding-top: 46px;">
+      <van-collapse v-model='activeNames'>
+        <van-collapse-item v-for="(list, owner) in repos" :name='owner' :key='owner'>
+          <div slot="title" style="color: #212121; font-size: 14px;">
+            {{owner}}
+            <van-tag plain type="primary">{{list.length}}</van-tag>
+          </div>
+          <router-link v-for="repo in list" :to="{ name: 'build', query: { owner, repo: repo.name } }" :key='repo.id'>
+            <van-cell :title="repo.name" is-link/>
+          </router-link>
+        </van-collapse-item>
+      </van-collapse>
+    </van-pull-refresh>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import _ from 'lodash'
-import { NavBar, Loading, Icon, Collapse, CollapseItem, Cell, CellGroup, Toast } from 'vant'
+import { NavBar, Loading, Icon, Collapse, CollapseItem, Cell, Tag, CellGroup, PullRefresh } from 'vant'
 import { getUserRepos } from '@/api/repo'
-Vue.use(NavBar).use(Loading).use(Icon).use(Collapse).use(CollapseItem).use(Cell).use(CellGroup)
+Vue.use(NavBar).use(Loading).use(Icon).use(Collapse).use(CollapseItem).use(Cell).use(Tag).use(CellGroup).use(PullRefresh)
 
 export default {
   name: 'home',
   data () {
     return {
+      isLoading: false,
       activeNames: ['1'],
       repos: {}
     }
   },
   methods: {
     fetchRepos: async function () {
-      Toast.loading({
-        mask: true,
-        duration: 0,
-        forbidClick: true
-      })
+      this.isLoading = true
       const repos = await getUserRepos()
       this.repos = _.groupBy(repos, 'owner')
-      Toast.clear()
+      const $this = this
+      _.debounce(() => {
+        $this.isLoading = false
+      }, 500)()
     }
   },
   created: function () {
