b4bf9d894678a0fdbdc8988e504899402199d192
yinfxs <yinfxs@gmail.com>
2018-10-04 01:07:27 +0800

    use cnpm

--- publish.sh
+++ publish.sh
@@ -2,8 +2,8 @@
 
 ver="0.0.20"
 tag="yinfxs/drone-client:$ver"
-cd ./ui && npm i && npm run build && cd ../
-cd ./f7 && npm i && npm run build && cd ../
+cd ./ui && cnpm i && npm run build && cd ../
+cd ./f7 && cnpm i && npm run build && cd ../
 GOOS=linux GOARCH=amd64 go build
 docker build -t $tag .
 docker push $tag
